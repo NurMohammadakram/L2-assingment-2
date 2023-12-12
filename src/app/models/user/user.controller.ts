baf3e0a09c81bc8bc21501ffc6c7320b515d335b
NurMohammadakram <nurmohammad19k8@gmail.com>
2023-12-13 00:45:34 +0600

    geting all user functionality done

--- src/app/models/user/user.controller.ts
+++ src/app/models/user/user.controller.ts
@@ -1,24 +1,41 @@
-import { Request, Response } from 'express'
-import { userServices } from './user.service'
+import { Request, Response } from 'express';
+import { userServices } from './user.service';
 
 const createUser = async (req: Request, res: Response) => {
   try {
-    const user = req.body
-    console.log(user)
-    const userData = await userServices.createUserService(user)
+    const user = req.body;
+    console.log(user);
+    const userData = await userServices.createUserService(user);
     res.status(200).json({
       success: true,
       messege: 'User created successfully!',
       data: userData,
-    })
+    });
   } catch (err) {
     res.status(500).json({
       success: false,
       messege: 'error happend',
-    })
+    });
   }
-}
+};
+
+const getAllUser = async (req: Request, res: Response) => {
+  try {
+    const userData = await userServices.getAllUserService();
+    res.status(200).json({
+      success: true,
+      message: 'all user retrieved',
+      data: userData,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: 'can not find users',
+    });
+  }
+};
 
 export const userControllers = {
   createUser,
-}
+  getAllUser,
+};
--- src/app/models/user/user.service.ts
+++ src/app/models/user/user.service.ts
@@ -1,15 +1,28 @@
-import { UserInterface } from './user.interface'
-import { UserModel } from './user.model'
+import { UserInterface } from './user.interface';
+import { UserModel } from './user.model';
 
 const createUserService = async (user: UserInterface) => {
   try {
-    const result = await UserModel.create(user)
-    return result
+    const result = await UserModel.create(user);
+    return result;
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
-}
+};
+
+const getAllUserService = async () => {
+  try {
+    const result = await UserModel.find(
+      {},
+      { username: 1, fullName: 1, age: 1, email: 1, address: 1 },
+    );
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 export const userServices = {
   createUserService,
-}
+  getAllUserService,
+};
