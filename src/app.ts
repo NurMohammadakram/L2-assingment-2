b7a694844e4a83d6bff6611fcd79f371df99d5fd
NurMohammadakram <nurmohammad19k8@gmail.com>
2023-12-18 18:28:00 +0600

    deployed in vercel

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,17 @@
-import express, { Application, Request, Response } from 'express'
-import cors from 'cors'
-import { userRouter } from './app/models/user/user.route'
-const app: Application = express()
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import { userRouter } from './app/models/user/user.route';
+const app: Application = express();
 
 // parsers
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 // routers
-app.use('/api/users', userRouter)
+app.use('/api/users', userRouter);
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
-})
+  res.send('now deploying to versel');
+});
 
-export default app
+export default app;
